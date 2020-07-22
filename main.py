1991e41dd7e935eb49264208a353acbf61d4e1ac
Ahmed_Gharib <A.Gharib89@yahoo.com>
2020-07-22 16:32:40 +0200

    adding doc string to nck function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from math import factorial as F
 
 def nck(n , k):
+    """N Chose K function to get the number of K combinations of N elements given by the binomial.
+    ARGS: n, k where n is the number of elements and k is the number of combinations we need to get from n
+    """
     return F(n) / (F(k) * F(n-k))
