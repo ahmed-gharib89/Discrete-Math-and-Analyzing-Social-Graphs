3d56e8d269c551b185087ed51a6f426f70c9fbf9
Ahmed_Gharib <A.Gharib89@yahoo.com>
2020-07-27 08:16:29 +0200

    Modifying nck function on main.py to return int istead of float

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ def nck(n , k):
     """N Chose K function to get the number of K combinations of N elements given by the binomial.
     ARGS: n, k where n is the number of elements and k is the number of combinations we need to get from n
     """
-    return F(n) / (F(k) * F(n-k))
+    return int(F(n) / (F(k) * F(n-k)))
