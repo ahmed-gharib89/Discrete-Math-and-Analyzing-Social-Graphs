7b9fcdeda57e4973bd62bbf4aa133f25ca4de2aa
Ahmed Gharib <a.gharib89@yahoo.com>
2020-07-29 00:48:15 +0200

    modified main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from math import factorial as F
 
-def nck(n , k):
+
+def nck(n, k):
     """N Chose K function to get the number of K combinations of N elements given by the binomial.
     ARGS: n, k where n is the number of elements and k is the number of combinations we need to get from n
     """
-    return int(F(n) / (F(k) * F(n-k)))
+    return int(F(n) / (F(k) * F(n - k)))
